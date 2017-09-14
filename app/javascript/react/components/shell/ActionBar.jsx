55cdef4f8c39de85da77186def9b79ad7c1a2dcb
Rohit Sharma <adamendeve@gmail.com>
2017-09-14 14:14:21 -0500

    feat: allow output device switching

--- app/javascript/react/components/shell/ActionBar.jsx
+++ app/javascript/react/components/shell/ActionBar.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import { observer }         from 'mobx-react'
+import { observer } from 'mobx-react'
 
 import {
-  Collapse, Navbar,  NavbarToggler, NavbarBrand,
-  Nav,      NavItem, NavLink, Badge, Row, Col, Button,
-  ButtonGroup
+  Collapse, Navbar, NavbarToggler, NavbarBrand,
+  Nav, NavItem, NavLink, Badge, Row, Col, Button,
+  ButtonGroup, ButtonDropdown, Dropdown, DropdownItem, DropdownMenu,
+  DropdownToggle
 } from 'reactstrap'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import LoadingSpinner from 'ui/shell/LoadingSpinner'
@@ -13,7 +14,7 @@ import fireEvent from 'helpers/FireEvent'
 
 const actionBarStyle = {
   backgroundColor: '#e8e8e8',
-  boxShadow:       '1px 0px 2px 0 rgba(0,0,0,0.25), 1px 0 6px 0 rgba(0,0,0,0.175)',
+  boxShadow: '1px 0px 2px 0 rgba(0,0,0,0.25), 1px 0 6px 0 rgba(0,0,0,0.175)',
   padding: 0,
   zIndex: 9999,
   border: 'none',
@@ -21,9 +22,9 @@ const actionBarStyle = {
 }
 
 const actionBarNotification = {
-  fontSize:      '70%',
+  fontSize: '70%',
   verticalAlign: 'top',
-  boxShadow:     '0 1px 2px 0 rgba(0,0,0,0.2)'
+  boxShadow: '0 1px 2px 0 rgba(0,0,0,0.2)'
 }
 
 const style = {
@@ -35,15 +36,15 @@ const style = {
     width: '100%',
     justifyContent: 'center'
   },
-  
+
   callBarCallText: {
     color: 'white',
     margin: '0 auto',
-    fontSize: '1.5em'
+    fontSize: '1.5em',
+    paddingLeft: 15
   },
   callBarBtn: {
-    color: 'white',
-    backgroundColor: 'gray',
+    backgroundColor: '#e8e8e8',
     border: 'none',
     marginRight: 5
   },
@@ -57,61 +58,109 @@ const style = {
 
 @observer
 export default class ActionBar extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    }
+  }
+
   showSidebar() {
     fireEvent('toggleSidebar')
   }
 
   componentDidMount() {
     // this.props.store.fetchInbox()
-    
+
   }
 
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
+
   renderBar() {
-    const { callBar, callBarDisable, callBarCallText, callBarBtn, callBarEndText, unmuteBtn } = style
-    const { isCalling, isConnected, selectMute, isMute, callTime } = this.props.callingStore
-      return (
-        <Navbar style={callBar}>
-          <Row>
-            <Col sm="8">
-              <p style={callBarCallText}>
-                <span className="icon icon-phone"> </span>
-                  { isCalling ? `Calling ${this.props.callingStore.contactName}` : 'Call Ended' }    
-                  <span style={{float: 'right'}}>{callTime}</span>      
-              </p>
-            </Col>
-              <Col sm="4">            
-                <ReactCSSTransitionGroup
-                  transitionName="callBar"
-                  transitionEnterTimeout={500}
-                  transitionLeaveTimeout={200}>               
-               { isCalling && isConnected 
-                ?     <ButtonGroup>    
-                        <Button 
-                          id="Popover1"
-                          style={callBarBtn} 
-                          onClick={() => this.props.callingStore.isDialPad(true)}>Dialpad</Button>
-                        <Button 
-                          style={selectMute ? unmuteBtn : callBarBtn} 
-                          onClick={() => isMute(!selectMute)}>{ selectMute ? 'Unmute' : 'Mute' }</Button>
-                        <Button 
-                          style={callBarBtn} 
-                          onClick={() => this.props.callingStore.hangUp()}>End Call</Button> 
-                      </ButtonGroup>
-                      : null } 
-                </ReactCSSTransitionGroup>
-              </Col>
-                      
-          </Row>
-        </Navbar>
-      )
+    const {
+      callBar,
+      callBarDisable,
+      callBarCallText,
+      callBarBtn,
+      callBarEndText,
+      unmuteBtn
+    } = style
+
+    const {
+      isCalling,
+      isConnected,
+      selectMute,
+      isMute,
+      callTime,
+      currentOutputDevice,
+      changeOutput,
+      outputDevices
+    } = this.props.callingStore
+
+    return (
+      <Navbar style={callBar}>
+        <Row>
+          <Col sm="8">
+            <p style={callBarCallText}>
+              <span className="icon icon-phone"> </span>
+              {isCalling ? `Calling ${this.props.callingStore.contactName}` : 'Call Ended'}
+              <span style={{ float: 'right' }}>{callTime ? callTime : null}</span>
+            </p>
+          </Col>
+          <Col sm="4">
+            <ReactCSSTransitionGroup
+              transitionName="callBar"
+              transitionEnterTimeout={500}
+              transitionLeaveTimeout={200}>
+              {isCalling && isConnected
+                ? <ButtonGroup>
+                  <ButtonDropdown style={{ margin: '0px 15px' }} isOpen={this.state.dropdownOpen} dropup toggle={this.toggle}>
+                    <DropdownToggle style={{ backgroundColor: 'transparent', color: 'white' }} caret size="sm">
+                      {currentOutputDevice ? currentOutputDevice : 'Output Devices'}
+                    </DropdownToggle>
+                    <DropdownMenu >
+                      {outputDevices
+                        ? outputDevices.map((device) =>
+                          <DropdownItem
+                            onClick={() => changeOutput(device.id, device.label)} >
+                            {device.label}
+                          </DropdownItem>)
+                        : null}
+                    </DropdownMenu>
+                  </ButtonDropdown>
+                  <Button
+                    id="Popover1"
+                    style={callBarBtn}
+                    onClick={() => this.props.callingStore.isDialPad(true)}>Dialpad</Button>
+                  <Button
+                    style={selectMute ? unmuteBtn : callBarBtn}
+                    onClick={() => isMute(!selectMute)}>{selectMute ? 'Unmute' : 'Mute'}</Button>
+                  <Button
+                    style={callBarBtn}
+                    onClick={() => this.props.callingStore.hangUp()}>End Call</Button>
+                </ButtonGroup>
+                : null}
+            </ReactCSSTransitionGroup>
+          </Col>
+        </Row>
+      </Navbar>
+    )
   }
 
   render() {
-    const {isCalling, callBarVisible } = this.props.callingStore
-    
+    const { isCalling, callBarVisible } = this.props.callingStore
+
     const actionBarStyle = {
       backgroundColor: isCalling ? '#5cb85c' : (!isCalling && callBarVisible ? '#d9534f' : '#e8e8e8'),
-      boxShadow:       '1px 0px 2px 0 rgba(0,0,0,0.25), 1px 0 6px 0 rgba(0,0,0,0.175)',
+      boxShadow: '1px 0px 2px 0 rgba(0,0,0,0.25), 1px 0 6px 0 rgba(0,0,0,0.175)',
       padding: 0,
       zIndex: 9999,
       border: 'none',
@@ -121,52 +170,52 @@ export default class ActionBar extends Component {
 
     return (
       <Navbar
-        style = { actionBarStyle }
-        fixed = 'bottom'
-        className = 'nav'
+        style={actionBarStyle}
+        fixed='bottom'
+        className='nav'
       >
-      <Row>
-        <Col sm="8">
-          <ReactCSSTransitionGroup
-            transitionName="callBar"
-            transitionEnterTimeout={500}
-            transitionLeaveTimeout={300}>
-            {callBarVisible &&
-            <div>{this.renderBar()}</div>}
-          </ReactCSSTransitionGroup>
-        </Col>
-        <Col sm="4">
-        <Nav className='ml-auto flex-row p-3' navbar>
-          <NavItem className='ml-4'>
-            <span className='icon icon-list mr-2'/>
-            <span>Tasks</span>
+        <Row>
+          <Col sm="8">
+            <ReactCSSTransitionGroup
+              transitionName="callBar"
+              transitionEnterTimeout={500}
+              transitionLeaveTimeout={300}>
+              {callBarVisible &&
+                <div>{this.renderBar()}</div>}
+            </ReactCSSTransitionGroup>
+          </Col>
+          <Col sm="4">
+            <Nav className='ml-auto flex-row p-3' navbar>
+              <NavItem className='ml-4'>
+                <span className='icon icon-list mr-2' />
+                <span>Tasks</span>
 
-            {/* <Badge color='success' style={actionBarNotification} pill> */}
-            {/*   7 */}
-            {/* </Badge> */}
-          </NavItem>
+                {/* <Badge color='success' style={actionBarNotification} pill> */}
+                {/*   7 */}
+                {/* </Badge> */}
+              </NavItem>
 
-          <NavItem className='ml-4'>
-            <span className='icon icon-voicemail mr-2'/>
-            <span>Voicemail</span>
+              <NavItem className='ml-4'>
+                <span className='icon icon-voicemail mr-2' />
+                <span>Voicemail</span>
 
-            {/* <Badge color='danger' style={actionBarNotification} pill> */}
-            {/*   4 */}
-            {/* </Badge> */}
-          </NavItem>
+                {/* <Badge color='danger' style={actionBarNotification} pill> */}
+                {/*   4 */}
+                {/* </Badge> */}
+              </NavItem>
 
-          <NavItem className='ml-4' onClick={::this.showSidebar}>
-            <span className='icon icon-chat mr-2'/>
-            <span>Messages</span>
+              <NavItem className='ml-4' onClick={::this.showSidebar}>
+            <span className='icon icon-chat mr-2' />
+              <span>Messages</span>
 
-            <Badge color='danger' style={actionBarNotification} hidden={this.props.store.totalUnread < 1} pill>
-              { this.props.store.totalUnread }
-            </Badge>
+              <Badge color='danger' style={actionBarNotification} hidden={this.props.store.totalUnread < 1} pill>
+                {this.props.store.totalUnread}
+              </Badge>
           </NavItem>
         </Nav>
         </Col>
         </Row>
-      </Navbar>
+      </Navbar >
     )
   }
 }
--- app/javascript/react/stores/CallingStore.jsx
+++ app/javascript/react/stores/CallingStore.jsx
@@ -1,55 +1,63 @@
 import { observable, action, computed, runInAction, autorun, toJS } from 'mobx'
-import {setter} from 'mobx-decorators'
+import { setter } from 'mobx-decorators'
 
-import _   from 'lodash'
+import _ from 'lodash'
 import xhr from 'helpers/XHR'
 import axios from 'axios'
 import moment from 'moment'
-import {isEmpty, padCharsStart} from 'lodash/fp'
+import { isEmpty, padCharsStart } from 'lodash/fp'
 class CallingStore {
-  @setter @observable isCalling = false
-  @setter @observable callBarVisible   = false
-  @observable studentID         = null
-  @observable contact           = null
-  @observable contactName       = null
-  @observable phoneNumber       = null
-  @observable connection        = null
+  
+  @observable studentID = null
+  @observable contact = null
+  @observable contactName = null
+  @observable phoneNumber = null
+  @observable connection = null
+  @observable isConnected = false
+  @observable currentOutputDevice = null
+  @observable outputDevices = []
 
+  @setter @observable isCalling = false
+  @setter @observable callBarVisible = false
   @setter @observable selectConferenceCall = false
   @setter @observable selectCall = false
   @setter @observable selectDialPad = false
-  @setter @observable selectMute    = false
-  @observable isConnected = false
-  @observable intervalHandler = null
- disposerTmr = null
+  @setter @observable selectMute = false
   @setter @observable callTime = null
+
+  intervalHandler = null
+  disposerTmr = null
+  callStartTime = null
 
   generateToken = () => {
     return xhr.get('/commo/capability_token')
-    .then((response) => {
-      return response
-    })
-    .catch((error) => {
-      console.log(error)
-    })
+      .then((response) => {
+        return response
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   }
 
   /* Calling from Browser */
   @action
-  initiateCall = async(contact, studentId) => {
+  initiateCall = async (contact, studentId) => {
+    this.callStartTime = null
+    this.setCallTime(null)
+    this.setSelectConferenceCall(false)
     const data = await this.generateToken()
     this.setIsCalling(true)
     this.setCallBarVisible(true)
 
     this.contactName = this.contact.refs[0].name
-    this.studentID   = studentId
+    this.studentID = studentId
     const phoneNumber = '6012128813'
     const token = data.data.token
     this.userId = data.data.user
 
     this.setupDevice(token)
     setTimeout(() =>
-    this.connect(phoneNumber), 2000)
+      this.connect(phoneNumber), 2000)
   }
 
   @action
@@ -57,43 +65,39 @@ class CallingStore {
     const params = {
       contact_id: this.contact.refs[0].id,
       student_id: this.studentID,
-      tocall:     '6012128183',
-      user_id:    this.userId
+      tocall: '6012128183',
+      user_id: this.userId
     }
- 
+
     this.connection = Twilio.Device.connect(params)
 
     this.connection.accept((conn) => {
-      console.log('accepted')
+      this.getOutputDevices()
       this.isConnected = true
-
+      console.log(Twilio.Device.audio.speakerDevices)
+      if (this.isConnected) {
+        this.callStartTime = Date.now()
+        this.intervalHandler = setInterval(action('SETTING CALL TIME', () => {
+          const t = getTime(this.callStartTime, Date.now())
+          this.setCallTime(t)
+        }), 1000)
+      }
     })
-
-          // set timer when calls are initiated
-          this.disposerTmr = autorun(() => {
-            if (this.isConnected) {
-              const callStartTime = Date.now()
-              this.intervalHandler = setInterval(action('SETTING CALL TIME', () => {
-                const t = getTime(callStartTime, Date.now())
-                console.log('CALL TIME => ', t)
-                this.setCallTime(t)
-              }), 1000)
-            } else {
-              this.intervalHandler = null
-            }
-          })
 
     this.connection.disconnect((conn) => {
       this.setIsCalling(false)
-      setTimeout(() => 
-      !this.isCalling && this.setCallBarVisible(false), 5000)
+      clearInterval(this.intervalHandler)
+      setTimeout(() =>
+        !this.isCalling && this.setCallBarVisible(false), 5000)
     })
 
     this.connection.reject((conn) => {
       this.setIsCalling(false)
-      setTimeout(() => 
-      this.setCallBarVisible(false), 5000)
+      setTimeout(() =>
+        this.setCallBarVisible(false), 5000)
     })
+
+
 
   }
 
@@ -114,41 +118,57 @@ class CallingStore {
     const params = {
       contact_id: this.contactID
     }
-    
+
     xhr.post('/commo/voice/mobile_call', {
       contact_id: this.contactID
     })
     .then((response) => {
+      console.log(response)
     })
     .catch((error) => {
+      console.log(error)
     })
   }
 
   @action
   initiateConferenceCall = async (contact, student_id) => {
     console.log(contact.refs[0].id)
-    this.contactID   = contact.refs[0].id
+    this.contactID = contact.refs[0].id
     this.contactName = contact.refs[0].name
     this.phoneNumber = contact.refs[0].phone
-    this.studentID   = student_id
-    
+    this.studentID = student_id
+
     const data = await this.generateToken()
     const token = data.data.token
     this.userId = data.data.user
 
-    console.log(await this.conferenceCall())
+    this.conferenceCall()
+  }
 
+  @action
+  getOutputDevices = () => {
+    Twilio.Device.audio.availableOutputDevices.forEach((device, id) => {
+      this.outputDevices.push({ label: device.label, id })
+    })
+  }
+
+  @action
+  changeOutput = (device, label) => {
+    Twilio.Device.audio.speakerDevices.set(device)
+    this.currentOutputDevice = label
   }
 
   @action
   hangUp = () => {
+    this.isConnected = false
     this.connection.disconnect()
     this.setIsCalling(false)
-    this.isConnected = false
-    this.disposerTmr()
-    setTimeout(() => 
-    this.setCallBarVisible(false), 5000)
+
+    setTimeout(() => {
+      this.setCallBarVisible(false);
+    }, 5000)
     this.isDialPad(false)
+    this.outputDevices = []
   }
 
   @action.bound
@@ -172,13 +192,13 @@ class CallingStore {
     try {
       const device = Twilio.Device.setup(token)
     }
-    catch(e) {
+    catch (e) {
       console.log(e)
     }
   }
 }
 
-function getTime (start, end) {
+function getTime(start, end) {
   var startTime = moment(start)
   var endTime = moment(end)
   var duration = moment.duration(endTime.diff(startTime))
