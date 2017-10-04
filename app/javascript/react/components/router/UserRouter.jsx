ef643b4b4c977a5bc7640767f039c44ca3a9410a
Joe Buza <joebm08@gmail.com>
2017-10-04 14:53:50 -0500

    feat: add user store

--- app/javascript/react/components/router/UserRouter.jsx
+++ app/javascript/react/components/router/UserRouter.jsx
@@ -3,15 +3,17 @@ import { BrowserRouter }        from 'react-router-dom'
 import { LastLocationProvider } from 'react-router-last-location'
 import { Provider as MobxProvider }             from 'mobx-react'
 
-import UserMain from 'ui/app/UserMain'
-
-import uiStore from 'stores/UiStore'
-
-const store = {uiStore}
+import UserMain  from 'ui/app/UserMain'
+import uiStore   from 'stores/UiStore'
+import userStore from 'stores/UserStore'
 
 const UserRouter = props => {
   // we inject ui related user props serverside and set to window var window.SSUser = props.user
   window.SSUser = props.user
+  userStore.setUser(props.user)
+
+  const store = {uiStore, userStore}
+
   return (
     <BrowserRouter>
       <MobxProvider {...store}>
