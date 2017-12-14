b5ecb5d6d3c731407cf3f3725e84961706c2c4d9
Joe Buza <joebm08@gmail.com>
2017-12-14 15:04:15 -0600

    feat: map modal

---
+++ app/javascript/react/components/shell/SmartTags/MapModal.jsx
@@ -0,0 +1,28 @@
+import React                from 'react'
+import PropTypes            from 'prop-types'
+import { 
+  Modal, 
+  ModalBody, 
+  ModalHeader
+} from 'reactstrap'
+
+MapModal.propTypes = {
+  toggle: PropTypes.func.isRequired,
+  src:    PropTypes.string.isRequired,
+  isOpen: PropTypes.bool.isRequired
+}
+export default function MapModal({ src, isOpen, toggle }) {
+  return (
+    <Modal 
+      isOpen={isOpen} 
+      toggle={toggle} 
+      size='lg' 
+      className='h-100 d-flex flex-column justify-content-center my-0'
+    >
+      <ModalHeader toggle={toggle} />
+      <ModalBody>
+        <img src={src} className='img-fluid mx-auto d-block'/>
+      </ModalBody>
+    </Modal>
+  )
+}
