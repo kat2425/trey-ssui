32f6939b9d5dfc81b427625598b79a50e2408a40
levi <lev@schoolstatus.com>
2018-01-17 14:59:38 -0600

    feat: add Export CSV button

--- app/javascript/react/components/shell/SmartTags/TopSection/index.jsx
+++ app/javascript/react/components/shell/SmartTags/TopSection/index.jsx
@@ -1,11 +1,12 @@
-import React        from 'react'
-import {observer}   from 'mobx-react'
-import renderIf     from 'render-if'
+import React           from 'react'
+import {observer}      from 'mobx-react'
+import renderIf        from 'render-if'
 
-import Wrapper      from './Wrapper'
-import Title        from './Title'
-import CloneButton  from './CloneButton'
-import DeleteButton from './DeleteButton'
+import Wrapper         from './Wrapper'
+import Title           from './Title'
+import CloneButton     from './CloneButton'
+import DeleteButton    from './DeleteButton'
+import ExportCSVButton from './ExportCSVButton'
 
 function TopSection({tagStore}){
   const { selectedTag }    = tagStore
@@ -15,6 +16,7 @@ function TopSection({tagStore}){
     <Wrapper>
       <Title tagStore={tagStore} />
       <div className='d-flex flex-row align-items-center justify-content-end' >
+        <ExportCSVButton tagStore={tagStore} />
         {renderIfModifiable(<CloneButton tagStore={tagStore} />)}
         <DeleteButton tagStore={tagStore} />
       </div>
