1884e0187f956989cce3bbda5099795a79800338
Joe Buza <joebm08@gmail.com>
2018-01-19 13:43:22 -0600

    fix: delete button

--- app/javascript/react/components/shell/SmartTags/TopSection/DeleteButton.jsx
+++ app/javascript/react/components/shell/SmartTags/TopSection/DeleteButton.jsx
@@ -1,42 +1,31 @@
-import React        from 'react'
-import { observer } from 'mobx-react'
-import { Button }   from 'reactstrap'
-import { Tooltip }  from 'antd'
+import React                   from 'react'
+import { observer }            from 'mobx-react'
+import { Button }              from 'reactstrap'
+import { Tooltip, Popconfirm } from 'antd'
 
 function DeleteButton({tagStore}){
   const { selectedTag } = tagStore
 
   if(!selectedTag) return null
   return (
-    <Tooltip title='Delete Tag'>
-      <Button
-        className = 'pl-2'
-        color     = 'danger'
-      >
-        <span className='icon icon-trash text-white' style={{marginRight: '4px'}}/>
-        Delete
-      </Button>
-    </Tooltip>
+    <Popconfirm
+      title      = "Are you sure?"
+      onConfirm  = {selectedTag.deleteTag}
+      okText     = 'OK'
+      cancelText = 'Cancel'
+    >
+      <Tooltip title='Delete Tag'>
+        <Button
+          className = 'pl-2'
+          color     = 'danger'
+          disabled  = {selectedTag.isDeleting}
+        >
+          <span className='icon icon-trash text-white' style={{marginRight: '4px'}}/>
+          {selectedTag.isDeleting ? 'Deleting...' : 'Delete' }
+        </Button>
+      </Tooltip>
+    </Popconfirm>
   )
 }
-
-// FIXME
-// Popconfirm surrounding button
-//    <Popconfirm
-//      title      = "Are you sure?"
-//      onConfirm  = {selectedTag.deleteTag}
-//      okText     = 'OK'
-//      cancelText = 'Cancel'
-//    >
-//      <Tooltip title='Delete Tag'>
-//        <Button
-//          className = 'pl-2'
-//          color     = 'danger'
-//        >
-//          <span className='icon icon-trash text-muted' style={{marginRight: '4px'}}/>
-//          Delete
-//        </Button>
-//      </Tooltip>
-//    </Popconfirm>
 
 export default observer(DeleteButton)
