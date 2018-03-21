23487730b8248c218f5867f208f5e11b1dbb650c
dphase <joshua.deere@gmail.com>
2018-03-21 05:54:26 -0500

    growth_cusp: add school selector

--- app/javascript/react/modules/logic/insights/GrowthCusp.jsx
+++ app/javascript/react/modules/logic/insights/GrowthCusp.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import ModuleHeader         from 'ui/shell/ModuleHeader'
 import VJSChart             from 'ui/vjs/VJSChart'
 import VJSICSelect          from 'ui/vjs/VJSICSelect'
+import userStore            from 'stores/UserStore'
 
 import fireEvent            from 'helpers/FireEvent'
 
@@ -13,10 +14,38 @@ export default class GrowthCusp extends Component {
     this.state = { params: {}, selected: {} }
   }
 
+  setTestType(val) {
+    const jrsValue = val ? val.value : '~NOTHING~'
+
+    this.setState({
+      params:   { ...this.state.params, test_type: [ jrsValue ] },
+      selected: { ...this.state.selected, test_type: val }
+    })
+  }
+
+  setSchoolFilter(val) {
+    const jrsValue = val ? val.value : '~NOTHING~'
+
+    this.setState({
+      params:   { ...this.state.params, school_id: [ jrsValue ] },
+      selected: { ...this.state.selected, school_id: val }
+    })
+  }
+
   render() {
     return (
       <div>
         <ModuleHeader title='Growth Cusp'>
+          <VJSICSelect
+            id            = 'user_schools'
+            inputPath     = '/public/VJS/ss_ui/shared/input_controls/user_schools'
+            selectedValue = {this.state.selected.school_id}
+            handleChange  = {::this.setSchoolFilter}
+            placeholder   = 'School'
+            setDefault    = {!userStore.user.isDistrictLevel}
+            clearable     = {userStore.user.isDistrictLevel}
+            width         = {300}
+          />
         </ModuleHeader>
 
         <div className='row'>
