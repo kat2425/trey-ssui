c4e7671d609696f72f96acddc731907f0ac1d527
dphase <joshua.deere@gmail.com>
2018-03-29 08:23:46 -0500

    assessment: add test version filter to ATI

--- app/javascript/react/modules/logic/assessment/ATI.jsx
+++ app/javascript/react/modules/logic/assessment/ATI.jsx
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
 
-import VJSChart  from 'ui/vjs/VJSChart'
-import fireEvent from 'helpers/FireEvent'
+import VJSChart             from 'ui/vjs/VJSChart'
+import VJSICSelect          from 'ui/vjs/VJSICSelect'
+import fireEvent            from 'helpers/FireEvent'
 
 export default class ATI extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      params:   {}, selected: {}
+    }
+  }
+
+  setYearFilter(val) {
+    const jrsValue = val ? val.value : '~NOTHING~'
+
+    this.setState({
+      params:   { ...this.state.params, school_year: [ jrsValue ] },
+      selected: { ...this.state.selected, school_year: val }
+    })
+  }
+
+  setTestVersionFilter(val) {
+    const jrsValue = val ? val.value : '~NOTHING~'
+
+    this.setState({
+      params:   { ...this.state.params, test_version: [ jrsValue ] },
+      selected: { ...this.state.selected, test_version: val }
+    })
   }
 
   render() {
@@ -18,6 +41,7 @@ export default class ATI extends Component {
             title       = 'ATI'
             className   = 'col-md-12'
             isTable     = {true}
+            params      = {this.state.params}
             linkOptions = {{
               events: {
                 click: (ev, link) => {
@@ -32,7 +56,16 @@ export default class ATI extends Component {
                 }
               }
             }}
-          />
+          >
+            <VJSICSelect
+              id            = 'test_version'
+              inputPath     = '/public/VJS/ss_ui/assessment/ati/test_version'
+              selectedValue = {this.state.selected.test_version}
+              handleChange  = {::this.setTestVersionFilter}
+              placeholder   = 'Test Type'
+              width         = {300}
+            />
+          </VJSChart>
         </div>
       </div>
     )
