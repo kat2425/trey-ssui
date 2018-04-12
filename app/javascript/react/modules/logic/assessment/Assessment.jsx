4bb7743f1219325977c9cab127b053640a97a113
dphase <joshua.deere@gmail.com>
2018-04-12 08:55:54 -0500

    assessments: add shared helper config

--- app/javascript/react/modules/logic/assessment/Assessment.jsx
+++ app/javascript/react/modules/logic/assessment/Assessment.jsx
@@ -37,6 +37,8 @@ import STARReading          from './STARReading'
 import Scantron             from './Scantron'
 import TCAP                 from './TCAP'
 
+import assessments          from 'helpers/Assessments'
+
 import userStore            from 'stores/UserStore'
 import _                    from 'lodash'
 
@@ -55,36 +57,8 @@ class Assessments extends Component {
 
   listOptions = () => {
     const { modules }  = userStore.user
-    const options      = [
-      { module: 'vjs_act',             value: 'act',             label: 'ACT'                },
-      { module: 'vjs_act_aspire',      value: 'act_aspire',      label: 'ACT Aspire'         },
-      { module: 'vjs_ati',             value: 'ati',             label: 'ATI'                },
-      { module: 'vjs_case21',          value: 'case21',          label: 'Case21'             },
-      { module: 'vjs_cogat',           value: 'cogat',           label: 'CogAT'              },
-      { module: 'vjs_dra',             value: 'dra',             label: 'DRA'                },
-      { module: 'vjs_dra_wa',          value: 'dra_wa',          label: 'DRA Word Analysis'  },
-      { module: 'vjs_dmac_staar',      value: 'dmac_staar',      label: 'STAAR',             },
-      { module: 'vjs_dmac_local',      value: 'dmac_local',      label: 'Local Assessments', },
-      { module: 'vjs_eduphoria',       value: 'eduphoria',       label: 'Eduphoria'          },
-      { module: 'vjs_las_links',       value: 'las_links',       label: 'LASLinks'           },
-      { module: 'vjs_maap',            value: 'maap',            label: 'MAAP'               },
-      { module: 'vjs_mastery_connect', value: 'mastery_connect', label: 'Mastery Connect'    },
-      { module: 'vjs_mct',             value: 'mct',             label: 'MCT'                },
-      { module: 'vjs_nwea',            value: 'nwea',            label: 'NWEA'               },
-      { module: 'vjs_ostp',            value: 'ostp',            label: 'OSTP'               },
-      { module: 'vjs_parcc',           value: 'parcc',           label: 'PARCC'              },
-      { module: 'vjs_psat',            value: 'psat_89',         label: 'PSAT 8/9'           },
-      { module: 'vjs_psat',            value: 'psat_nm',         label: 'PSAT NM'            },
-      { module: 'vjs_satp',            value: 'satp',            label: 'SATP'               },
-      { module: 'vjs_scantron',        value: 'scantron',        label: 'Scantron'           },
-      { module: 'vjs_tcap',            value: 'tcap',            label: 'TCAP'               },
-      { module: 'vjs_renplace',        value: 'accel_reader',    label: 'Accelerated Reader' },
-      { module: 'vjs_renplace',        value: 'star_early_lit',  label: 'STAR Early Lit'     },
-      { module: 'vjs_renplace',        value: 'star_math',       label: 'STAR Math'          },
-      { module: 'vjs_renplace',        value: 'star_reading',    label: 'STAR Reading'       },
-    ]
 
-    return _.filter(options, (o) => userStore.hasModules(o.module))
+    return _.filter(assessments, (o) => userStore.hasModules(o.module))
   }
 
   handleChange(selectedOption) {
@@ -155,8 +129,9 @@ class Assessments extends Component {
                 Click the menu in the top right corner. From the drop-down menu,
                 select an assessment package you'd like to view.
               </EmptyMessage>
-              )
-          }}/>
+            )
+          }}
+          />
         </Switch>
       </div>
     )
