137b7d0f2037cd4d19191dc6f780804ddc133778
dphase <joshua.deere@gmail.com>
2018-04-18 12:17:15 -0500

    engagement history: school/method filters

--- app/javascript/react/modules/channel/History.jsx
+++ app/javascript/react/modules/channel/History.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 
 import VJSChart             from 'ui/vjs/VJSChart'
+import VJSICSelect          from 'ui/vjs/VJSICSelect'
+import Select               from 'react-virtualized-select'
+
 import ModuleHeader         from 'ui/shell/ModuleHeader'
 import userStore            from 'stores/UserStore'
 import fireEvent            from 'helpers/FireEvent'
@@ -8,6 +11,36 @@ import fireEvent            from 'helpers/FireEvent'
 export default class CallHistory extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      params:   {}, selected: {}
+    }
+  }
+
+  setTypeFilter(val) {
+    const jrsValue = val ? val.value : '~NOTHING~'
+
+    this.setState({
+      params:   { ...this.state.params, contact_type: [ jrsValue ] },
+      selected: { ...this.state.selected, contact_type: val }
+    })
+  }
+
+  setSchoolFilter(val) {
+    const jrsValue = val ? val.value : '~NOTHING~'
+
+    this.setState({
+      params:   { ...this.state.params, school_id: [ jrsValue ] },
+      selected: { ...this.state.selected, school_id: val }
+    })
+  }
+
+  contactTypes = () => {
+    return [
+      { value: 'Call',  label: 'Call'  },
+      { value: 'Text',  label: 'Text'  },
+      { value: 'Email', label: 'Email' }
+    ]
   }
 
   getHistoryPath = () => {
@@ -19,9 +52,35 @@ export default class CallHistory extends Component {
   }
 
   render() {
+    const { selected } = this.state
+    const typeValue    = selected.contact_type && selected.contact_type.value
+
     return (
       <div>
-        <ModuleHeader title='Engagement History'/>
+        <ModuleHeader title='Engagement History'>
+          <VJSICSelect
+            id            = 'user_schools'
+            inputPath     = '/public/VJS/ss_ui/shared/input_controls/user_schools'
+            selectedValue = {this.state.selected.school_id}
+            handleChange  = {::this.setSchoolFilter}
+            placeholder   = 'School'
+            setDefault    = {!userStore.user.isDistrictLevel}
+            clearable     = {userStore.user.isDistrictLevel}
+            width         = {300}
+          />
+
+          <div style={{width: 175}} className='ml-2'>
+            <Select
+              name        = 'contact-type-menu'
+              value       = {typeValue}
+              onChange    = {::this.setTypeFilter}
+              maxHeight   = {325}
+              options     = {this.contactTypes()}
+              clearable   = {true}
+              placeholder = 'Engagement Method'
+            />
+          </div>
+        </ModuleHeader>
 
         <div className='row'>
           <VJSChart
@@ -30,6 +89,7 @@ export default class CallHistory extends Component {
             title       = 'Student Detail'
             className   = 'col-md-12'
             isTable     = {true}
+            params      = {this.state.params}
             linkOptions = {{
               events: {
                 click: (ev, link) => {
