e77bf6393f1bd117ab4d9e66f2e5735da0fcef9a
Linell <tlbonnette@gmail.com>
2018-04-20 15:39:02 -0500

    feat: add basic district expiration nag
    Add a basic nag to let users know that their district is about to
    expire.

--- app/models/district.rb
+++ app/models/district.rb
@@ -25,4 +25,40 @@ class District < Sequel::Model(:districts)
   rescue
     []
   end
+
+  # Expiration Informatoin                                                      {{{
+  # -------------------------------------------------------------------------------
+  def expiration_status
+    case
+      when days_until_expiration > 60
+        :good
+      when days_until_expiration > 0
+        :warning
+      when ((days_until_expiration > -30) || Date.today < grace_date)
+        :grace
+      else :expired
+    end
+  end
+
+  def days_until_expiration
+    (expires_date - Date.today).to_i
+  rescue Exception => e
+    if e.message =~ /undefined method `-' for nil:NilClass/
+      0
+    else
+      raise e
+    end
+  end
+
+  def expired?
+    expiration_status == :expired
+  rescue Exception => e
+    true
+  end
+
+  def grace?
+    expiration_status == :grace
+  end
+
+  # }}}
 end
