705c03cc1811d56914ba6f715f80275223b159da
dphase <joshua.deere@gmail.com>
2018-08-28 08:53:48 -0500

    assessments: add school year toggle to OSTP

--- app/javascript/react/modules/logic/assessment/OSTP.jsx
+++ app/javascript/react/modules/logic/assessment/OSTP.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 
-import ModuleHeader         from 'ui/shell/ModuleHeader'
 import VJSChart             from 'ui/vjs/VJSChart'
 import VJSICSelect          from 'ui/vjs/VJSICSelect'
 
@@ -13,10 +12,18 @@ export default class OSTP extends Component {
     this.state = { params: {}, selected: {} }
   }
 
+  setYearFilter(val) {
+    const jrsValue = val ? val.value : '~NOTHING~'
+
+    this.setState({
+      params:   { ...this.state.params, school_year: [ jrsValue ] },
+      selected: { ...this.state.selected, school_year: val }
+    })
+  }
+
   render() {
     return (
       <div>
-
         <div className='row'>
           <VJSChart
             id          = 'ostp-student-detail'
@@ -39,7 +46,19 @@ export default class OSTP extends Component {
                 }
               }
             }}
-          />
+          >
+            <VJSICSelect
+              id            = 'school_year'
+              inputPath     = '/public/VJS/ss_ui/shared/input_controls/district_dataset_years/report'
+              selectedValue = {this.state.selected.school_year}
+              handleChange  = {::this.setYearFilter}
+              clearable     = {false}
+              setDefault    = {true}
+              placeholder   = 'Year'
+              width         = {100}
+              params        = {{ dataset: ['vw_ostp'] }}
+            />
+          </VJSChart>
         </div>
       </div>
     )
