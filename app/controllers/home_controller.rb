16309ae285b8b4d804667e05b789d6032b62be18
dphase <joshua.deere@gmail.com>
2018-09-24 10:11:58 -0500

    change order of user expiration login logic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,11 @@ class HomeController < ApplicationController
   def index
     authenticate!
 
+    redirect_to('/cj/blog') if user.is_superuser?
+
     # redirect for expired districts
     if user&.district.expired?
       redirect_to '/subscription_expired'
     end
-
-    redirect_to('/cj/blog') if user.is_superuser?
   end
 end
