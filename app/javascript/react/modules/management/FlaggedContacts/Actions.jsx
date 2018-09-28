8083713c7ad4075a91af9045f475f00ce798e3d9
levi <levi@schoolstatus.com>
2018-09-28 13:30:23 -0500

    fix: add confirm before unflagging contacts

--- app/javascript/react/modules/management/FlaggedContacts/Actions.jsx
+++ app/javascript/react/modules/management/FlaggedContacts/Actions.jsx
@@ -1,13 +1,15 @@
 import React           from 'react'
 import { observer }    from 'mobx-react'
 import SSButton        from 'ui/shell/SSButton'
-import { Badge }       from 'antd'
+import {
+  Badge, Popconfirm
+}                      from 'antd'
 import { MdEventNote } from 'react-icons/lib/md'
 
 const Actions = ({contact, store}) => (
   <div>
-    <Badge count={contact.flagsCount} className='mr-2'> 
-      <SSButton 
+    <Badge count={contact.flagsCount} className='mr-2'>
+      <SSButton
         disabled = {contact.isUnFlagging}
         onClick  = {() => store.toggleViewNotes(contact)}
         size     = 'sm'
@@ -15,17 +17,21 @@ const Actions = ({contact, store}) => (
         <MdEventNote size={14}/> View Notes
       </SSButton>
     </Badge>
-
-    <SSButton 
-      onClick   = {contact.unFlagNumber}
-      iconClass = 'icon icon-flag'
-      color     = 'danger'
-      className = 'ml-3'
-      size      = 'sm'
-      loading   = {contact.isUnFlagging}
-    >
-      Unflag
-    </SSButton>
+    <Popconfirm
+      title='Are you sure want to unflag this contact?'
+       onConfirm={contact.unFlagNumber}
+       okText='Unflag'
+       cancelText='Cancel'>
+      <SSButton
+        iconClass = 'icon icon-flag'
+        color     = 'danger'
+        className = 'ml-3'
+        size      = 'sm'
+        loading   = {contact.isUnFlagging}
+      >
+        Unflag
+      </SSButton>
+    </Popconfirm>
   </div>
 )
 
--- app/javascript/react/modules/management/FlaggedContacts/TopBar.jsx
+++ app/javascript/react/modules/management/FlaggedContacts/TopBar.jsx
@@ -1,13 +1,14 @@
-import React        from 'react'
-import { observer } from 'mobx-react'
-import SSButton     from 'ui/shell/SSButton'
-import Search       from './Search'
+import React          from 'react'
+import { observer }   from 'mobx-react'
+import SSButton       from 'ui/shell/SSButton'
+import { Popconfirm } from 'antd'
+import Search         from './Search'
 
-const TopBar = ({store}) => ( 
+const TopBar = ({store}) => (
   <div className='mb-4 d-flex justify-content-between align-items-center'>
     <UnFlagAllButton store={store} />
     <Search store={store} style={{width: '50%'}}/>
-    <SSButton 
+    <SSButton
       iconClass = 'icon icon-cw'
       disabled  = {store.isLoading}
       onClick   = {store.fetchFlaggedContacts}
@@ -19,13 +20,18 @@ const TopBar = ({store}) => (
 
 const UnFlagAllButton = observer(({store}) => (
   <div className='d-inline-flex align-items-center'>
-    <SSButton
-      color    = 'primary'
-      onClick  = {store.handleOnUnflagSelectedContacts}
-      disabled = {!store.hasSelected}
-    >
-      Unflag All Selected {getSelectedCount(store)}
-    </SSButton>
+    <Popconfirm
+      title='Are you sure want to unflag selected contacts?'
+       onConfirm={store.handleOnUnflagSelectedContacts}
+       okText='Unflag'
+       cancelText='Cancel'>
+      <SSButton
+        color    = 'primary'
+        disabled = {!store.hasSelected}
+      >
+        Unflag All Selected {getSelectedCount(store)}
+      </SSButton>
+    </Popconfirm>
   </div>
 ))
 
