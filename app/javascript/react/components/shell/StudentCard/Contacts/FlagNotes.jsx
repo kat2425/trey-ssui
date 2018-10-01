db40406a6a58eab909ae9c4e2e0079ab63e3805c
levi <levi@schoolstatus.com>
2018-10-01 10:41:53 -0500

    fix: stop breaking words on flag notes

--- app/javascript/react/components/shell/StudentCard/Contacts/FlagNotes.jsx
+++ app/javascript/react/components/shell/StudentCard/Contacts/FlagNotes.jsx
@@ -4,23 +4,31 @@ import Table        from 'ui/shell/AntdTable'
 
 
 const FlagNotes = ({contact}) => {
-  const columns = [{
-    title:     'Note',
-    dataIndex: 'note',
-    key:       'note',
-    width:     '50%'
-  },{
-    title:     'Created At',
-    dataIndex: 'createdDate',
-    key:       'createdDate',
-  },{
-    title:     'Creator',
-    dataIndex: 'creator',
-    key:       'creator',
-  }]
+  const columns = [
+    {
+      title:  'Note',
+      key:    'note',
+      width:  '50%',
+      render: ({ id, note }) => (
+        <div key={id} style={{ wordBreak: 'normal' }}>
+          {note}
+        </div>
+      )
+    },
+    {
+      title:     'Created At',
+      dataIndex: 'createdDate',
+      key:       'createdDate'
+    },
+    {
+      title:     'Creator',
+      dataIndex: 'creator',
+      key:       'creator'
+    }
+  ]
 
   return (
-    <Table 
+    <Table
       dataSource = {contact.dataSource}
       columns    = {columns}
       pagination = {false}
